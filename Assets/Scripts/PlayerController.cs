4343e471abf8f7b13b7715ca0de70931add171dd
Nata-Bogdan <hatawkaflame@gmail.com>
2025-01-29 11:50:45 +0200

    firstday

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public Text ScoreText;
+    public Text WinText;
+    public GameObject Gate;
+    public Rigidbody rb;
+    public int Score;
+
+    void SetScoreText() 
+    {
+        ScoreText.text = "Score: " + Score.ToSpring();
+        if (Score >= 5) {
+            WinText.text = "You Won! Press R to restart or ESC to quit"
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent <RigidBody>()
+        Score = 0;
+        SetScoreText();
+        WinText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new.Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * Speed);
+        
+        //restart
+        if (Input.GetKeyDown()
+    }
+}
